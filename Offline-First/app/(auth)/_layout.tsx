968ab09adf51b03893ac1d09fb1510e31c27bd44
teklu <tekluabayneh@gmail.com>
2026-05-12 22:23:39 +0300

    feat: implement authentication screens and user flow

--- Offline-First/app/(auth)/_layout.tsx
+++ Offline-First/app/(auth)/_layout.tsx
@@ -1,6 +1,16 @@
-
-import { Stack } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Redirect, router, Stack } from 'expo-router';
 export default function AuthLayout() {
+
+  const IsuserLoggedIn = AsyncStorage.getItem("userLoggedIn")
+  if (!IsuserLoggedIn) {
+    // router.replace("/(auth)")
+    <Redirect href="/(auth)" />;
+  } else {
+    // router.replace("/(tabs)")
+    <Redirect href="/(tabs)" />;
+  }
+
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
--- Offline-First/app/(auth)/index.tsx
+++ Offline-First/app/(auth)/index.tsx
@@ -2,15 +2,16 @@ import { Text, View, Pressable } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons';
 import AuthenticateWithBiometrix from '@/hooks/BiometrixAuth';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { router } from 'expo-router';
 
 export default function AuthScreen() {
 
   return (
-    // background-color: #131315
-    <SafeAreaView className='bg-vault-black flex-1 items-center p-6 bg-black'>
+    < SafeAreaView className='bg-vault-black flex-1 items-center p-6 bg-black' >
 
       {/* Mesh Background Mimic (Top Glow) */}
-      <View className="absolute top-0 left-0 right-0 h-96 opacity-20 bg-indigo-600 rounded-full blur-[100px] -translate-y-48" />
+      < View className="absolute top-0 left-0 right-0 h-96 opacity-20 bg-indigo-600 rounded-full blur-[100px] -translate-y-48" />
       <Text className='text-white text-4xl font-bold tracking-tight'>Secure Access</Text>
 
       <View className='flex flex-row gap-2 items-center border border-emerald-500/20 bg-emerald-500/10 mt-4 p-2 px-4 rounded-full'>
@@ -26,14 +27,19 @@ export default function AuthScreen() {
       <Text className='text-slate-500 px-10 text-center mt-6 leading-6'>
         Unlock your digital vault using registered biometrics.
       </Text>
+      <Pressable onPress={() => {
+        AsyncStorage.setItem("userLoggedIn", "true")
+        router.replace("/(tabs)");
+      }}>
+        <Text className='text-white'>fake login</Text>
+      </Pressable>
+
 
       <View className='flex-row mt-auto mb-10 gap-5'>
 
         {/* FaceID Card - The "Glass Vault" Style */}
-        <Pressable className='glass-vault flex-1 bg-white/5 border border-white/10 p-6 items-center rounded-3xl shadow-2xl'
-
+        <Pressable className='glass-vault flex-1 bg-white/5 border border-white/10 p-6 items-center rounded-3xl'
           onPress={() => AuthenticateWithBiometrix()}
-
         >
           <View className="bg-indigo-500/10 p-4 rounded-2xl mb-4">
             <MaterialCommunityIcons name="face-recognition" size={42} color="#818cf8" />
@@ -43,16 +49,18 @@ export default function AuthScreen() {
         </Pressable>
 
         {/* Fingerprint Card - The "Glass Vault" Style */}
-        <Pressable className='glass-vault flex-1 bg-white/5 border border-white/10 p-6 items-center rounded-3xl shadow-2xl'>
+        <Pressable className='glass-vault flex-1 bg-white/5 border border-white/10 p-6 items-center rounded-3xl'
+          onPress={() => AuthenticateWithBiometrix()}
+        >
           <View className="bg-indigo-500/10 p-4 rounded-2xl mb-4">
             <Ionicons name="finger-print-outline" size={42} color="#818cf8" />
           </View>
           <Text className='text-white font-semibold text-lg'>TouchID</Text>
           <Text className='text-slate-500 text-xs mt-1'>Biometric</Text>
         </Pressable>
-
       </View>
-    </SafeAreaView>
+
+    </SafeAreaView >
   );
 }
 
