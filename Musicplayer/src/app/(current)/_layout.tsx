28c4dac0154df9ccf5b039fcdd556281eccefa38
teklu <tekluabayneh@gmail.com>
2026-06-03 14:12:46 +0300

    refactor: adjust routing and layout properties for current route group

--- Musicplayer/src/app/(current)/_layout.tsx
+++ Musicplayer/src/app/(current)/_layout.tsx
@@ -1,9 +1,16 @@
-
-import { Tabs, Stack } from 'expo-router';
-
+import { Stack } from 'expo-router';
 
 export default function TabLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen
+        name="current"
+        options={{
+          title: "Settings",
+          animation: 'slide_from_bottom',
+          animationDuration: 6000,
+        }}
+      />
+    </Stack>
   );
 }
