8096b81c2841849f25b645f9e38f5221d0c6bbbb
teklu <tekluabayneh@gmail.com>
2026-06-03 14:12:45 +0300

    refactor: update navigation layout configuration for artists group

--- Musicplayer/src/app/(artists)/_layout.tsx
+++ Musicplayer/src/app/(artists)/_layout.tsx
@@ -1,9 +1,23 @@
 
-import { Tabs, Stack } from 'expo-router';
+import { Stack } from 'expo-router';
 
 
 export default function TabLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+
+      <Stack.Screen
+        name="chat-settings"
+        options={{
+          title: "Settings",
+          // This forces the slide-up on open, and slide-down on back/close
+          animation: 'slide_from_bottom',
+          animationDuration: 600,
+          // Optional: Makes it look like a futuristic native card modal on iOS
+          presentation: 'modal'
+        }}
+
+      />
+    </Stack>
   );
 }
