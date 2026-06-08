f10cbeebeacd414e29360ed4f10fd016602cdf30
teklu <tekluabayneh@gmail.com>
2026-06-08 17:12:42 +0300

    update artists index screen logic

--- Musicplayer/src/app/(artists)/index.tsx
+++ Musicplayer/src/app/(artists)/index.tsx
@@ -1,17 +1,29 @@
-import { View } from "react-native";
+import { Modal, View } from "react-native";
 import { NavigationBar } from "@/components/ui/NavigationBar";
 import ArtistVewingPlayScreen from "@/components/ui/CurrentlyVewingArtists";
 import ArtistsList from "@/components/ui/ArsistList";
+import ArtistScreen2 from "./CurrentlyPlayingScreen";
+import { useMusic } from "@/components/context/Music";
 
 // TODO 
 // musing playing functinlity must be using context as it allow us flexblity when sharing props
-export default function ArtistScreen({ pause, setPause }) {
+export default function ArtistScreen() {
+  const { showPlayer, setShowPlayer } = useMusic()
   return (
     <>
       <View className="flex-1 mt-6">
         <NavigationBar />
-        <ArtistVewingPlayScreen pause={pause} setPause={setPause} />
+        <ArtistVewingPlayScreen />
         <ArtistsList />
+
+        <Modal
+          visible={showPlayer}
+          animationType="slide"        // ← this gives you the smooth bottom-to-top animation
+          presentationStyle="fullScreen"
+          onRequestClose={() => setShowPlayer(false)}
+        >
+          <ArtistScreen2 onClose={() => setShowPlayer(false)} />
+        </Modal>
       </View>
     </>
   );
